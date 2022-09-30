9e4953057c65e840f8f4fc7fba0536309ff62a8e
Tabaré Sánchez <tabaregabriel@outlook.com.ar>
2022-09-30 17:00:33 -0300

    x

--- programacion_1/TP_3/act3.c
+++ programacion_1/TP_3/act3.c
@@ -2,3 +2,22 @@
  se desea leer el archivo hasta el final e indicar la cantidad de valores
  iguales a 12 o 15 que tiene el mismo.
  Luego imprimir esas cantidades en la pantalla.*/
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *archivo;
+    int cant = 0;
+    int num;
+    archivo = fopen("archivoDeEnteros.txt", "r");
+    while (!feof(archivo))
+    {
+        fscanf(archivo, "%d", &num);
+        if (num == 12 || num == 15)
+            cant++;
+    }
+    printf("La cantidad de valores iguales a 12 o 15 es: %d", cant);
+    fclose(archivo);
+    return 0;
+}
