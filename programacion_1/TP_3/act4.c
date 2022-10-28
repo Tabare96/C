8cb931cf726e062f4fefc6470f03cbfa29069a6e
Tabaré Sánchez <tabaregabriel@outlook.com.ar>
2022-10-28 14:08:29 -0300

    sad

--- programacion_1/TP_3/act4.c
+++ programacion_1/TP_3/act4.c
@@ -1,1 +1,21 @@
 //Explique dos instrucciones que se usan para el manejo de punteros.
+/*
+El símbolo * se usa para DEFINIR un tipo puntero
+
+El símbolo & se usa para indicar la dirección de una variable
+
+el símbolo * se usa para ACCEDER al valor de un puntero
+
+el símbolo & se usa en la invocación de funciones,de forma que
+se pone delante de los párametros de E/S
+
+Cuando un parámetro es de E/S en el prototipo y cabecera se indica 
+con un tipo puntero, esto es, poniendo un * delante del nombre
+
+En el cuerpo de la función los argumentos que son funciones se 
+USAN con un * delante
+
+Los & sólo se usan en el programa principal
+
+Los * sólo se usan en las funciones
+*/
--- programacion_1/TP_3/anotaciones.c
+++ programacion_1/TP_3/anotaciones.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 
-int factorial(int n)
+// int factorial(int n)
+// {
+//     if ((n == 0) || (n == 1))
+//         return (1);
+//     else
+//         return (n * factorial(n - 1));
+// }
+
+// int main()
+// {
+//     int n;
+//     printf("Ingrese un numero: ");
+//     scanf("%d", &n);
+//     printf("El factorial de %d es: %d", n, factorial(n));
+//     return 0;
+// }
+
+void intercambia(int *x, int *y)
 {
-    if ((n == 0) || (n == 1))
-        return (1);
-    else
-        return (n * factorial(n - 1));
+    int z = x;
+
+    z = *x;
+    *x = *y;
+    *y = z;
 }
 
 int main()
 {
-    int n;
-    printf("Ingrese un numero: ");
-    scanf("%d", &n);
-    printf("El factorial de %d es: %d", n, factorial(n));
-    return 0;
+    int a = 7;
+    int b = 8;
+
+    printf("a= %d  b= %d\n", a, b);
+    intercambia(&a, &b);
+    printf("a= %d  b= %d\n\n", a, b);
+
+
+
+
+    int c = 1;
+    int d = 2;
+
+    printf("c= %d  d= %d\n", c, d);
+    int e = c;
+    c = d;
+    d = e;
+    printf("c= %d  d= %d", c, d);
+
+
 }
